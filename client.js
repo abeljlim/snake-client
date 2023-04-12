d52500cca864b6fe0bbb81d6dbcb4ad9ffc68150
abeljlim <subrosian@hotmail.com>
2023-04-12 15:33:37 -0700

    Moved IP and port to constants in constants.js from client.js

--- client.js
+++ client.js
@@ -1,27 +1,28 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
-    host: "localhost",// IP address here,
-    port: 50541// PORT number here,
+    host: IP,// IP address here,
+    port: PORT// PORT number here,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-  
+
   conn.on("data", data => {
     console.log("Server says: ", data);
   });
 
   conn.on('connect', () => {
     console.log(`Successfully connected to game server`);
-   
+
     conn.write(`Name: AJL`);
   });
   conn.on('connect', () => {
     // console.log(`Successfully connected to game server`);
-   
+
     // conn.write(`Move: up`);
     // setInterval(() => conn.write(`Move: up`), 50);
   });
