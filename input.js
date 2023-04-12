72e864feff2c3c2a86a77dcea06904b2f111be23
abeljlim <subrosian@hotmail.com>
2023-04-12 15:24:59 -0700

    handling input using canned messages

--- input.js
+++ input.js
@@ -3,47 +3,34 @@
 let currentlyTyping = false;
 
 const handleUserInput = function(data) {
-
-  if (currentlyTyping) {
-    process.stdin.on('data', (data) => {
-    });
-    return;
-  }
-
-  if (data === '\u000d') {
-    if (!currentlyTyping) {
-      currentlyTyping = true;
-      process.stdout.write(`Say: `);
-      return;
-    }
-    const name = data.toString(); // can't use data.replace(/^\s+|\s+$/g, '') because data is a buffer and not a string
-    connection.write(`Say: ${name.replace(/^\s+|\s+$/g, '')}`);
-  }
-
   switch (data) {
     case '\u0003':
       process.exit();
       break;
-    case 'w': 
-    connection.write("Move: up");
-
+    case 'w':
+      connection.write("Move: up");
+      break;
+    case 'a':
+      connection.write("Move: left");
+      break;
+    case 's':
+      connection.write("Move: down");
+      break;
+    case 'd':
+      connection.write("Move: right");
+      break;
+    case 'z':
+      connection.write("Say: Hey everyone!");
+      break;
+    case 'x':
+      connection.write("Say: Outta my way");
+      break;
+    case 'c':
+      connection.write("Say: Gimme the food!");
+      break;
+    default:
+      break;
   }
-  if (data === '\u0003') {
-    process.exit();
-  }
-  if (data === 'w') {
-    connection.write("Move: up");
-  }
-  if (data === 'a') {
-    connection.write("Move: left");
-  }
-  if (data === 's') {
-    connection.write("Move: down");
-  }
-  if (data === 'd') {
-    connection.write("Move: right");
-  }
-
 };
 
 // Stores the active TCP connection object.
